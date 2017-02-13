229b2b215e5de44aa3af961969a4733b20608258
Philip Wood <philip.m.wood@gmail.com>
2017-02-13 20:34:36 +0000

    Fix serialization

---
+++ src/BinaryMash.Responses.Tests/Specs/PayloadResponseSpecs.cs
@@ -0,0 +1,65 @@
+﻿namespace BinaryMash.Responses.Tests.Specs
+{
+    using Newtonsoft.Json;
+    using Shouldly;
+    using TestStack.BDDfy;
+    using Xunit;
+
+    public class ResponseSpecscs
+    {
+        Response<MyTestClass> originalResponse;
+
+        string serializedResponse;
+
+        Response<MyTestClass> deserializedResponse; 
+
+        [Fact]
+        public void DeserializationWithJsonDotNet()
+        {
+            this.Given(_ => GivenAResponseObjectWithErrors())
+                .When(_ => WhenWeSerializeIt())
+                .And(_ => WhenWeDeserializeItAgain())
+                .Then(_ => ThenTheDeserializedResponseMatchesTheOriginalResponse())
+                .BDDfy();
+        }
+
+        private void GivenAResponseObjectWithErrors()
+        {
+            originalResponse = BuildResponse
+                .WithPayload(new MyTestClass { SomeValue = "ThisIsAValue"})
+                .AndWithErrors(new[]
+                    {
+                        new Error("123", "456"),
+                        new Error("789", "012")
+                    })
+                .Create();
+        }
+
+        private void WhenWeSerializeIt()
+        {
+            serializedResponse = JsonConvert.SerializeObject(originalResponse);
+        }
+
+        private void WhenWeDeserializeItAgain()
+        {
+            deserializedResponse = JsonConvert.DeserializeObject<Response<MyTestClass>>(serializedResponse);
+        }
+
+        private void ThenTheDeserializedResponseMatchesTheOriginalResponse()
+        {
+            deserializedResponse.Errors.Count.ShouldBe(originalResponse.Errors.Count);
+
+            foreach(var error in originalResponse.Errors)
+            {
+                deserializedResponse.Errors.ShouldContain(e => e.Code == error.Code && e.Message == error.Message);
+            }
+
+            deserializedResponse.Payload.SomeValue.ShouldBe(originalResponse.Payload.SomeValue);
+        }
+
+        class MyTestClass
+        {
+            public string SomeValue { get; set; }
+        }
+    }
+}
---
+++ src/BinaryMash.Responses.Tests/Specs/ResponseSpecs.cs
@@ -0,0 +1,57 @@
+﻿namespace BinaryMash.Responses.Tests.Specs
+{
+    using Newtonsoft.Json;
+    using Shouldly;
+    using TestStack.BDDfy;
+    using Xunit;
+
+    public class ResponseSpecs
+    {
+        Response originalResponse;
+
+        string serializedResponse;
+
+        Response deserializedResponse; 
+
+        [Fact]
+        public void DeserializationWithJsonDotNet()
+        {
+            this.Given(_ => GivenAResponseObjectWithErrors())
+                .When(_ => WhenWeSerializeIt())
+                .And(_ => WhenWeDeserializeItAgain())
+                .Then(_ => ThenTheDeserializedResponseMatchesTheOriginalResponse())
+                .BDDfy();
+        }
+
+        private void GivenAResponseObjectWithErrors()
+        {
+            originalResponse = BuildResponse
+                .WithNoPayload()
+                .AndWithErrors(new[]
+                    {
+                        new Error("123", "456"),
+                    })
+                .Create();
+        }
+
+        private void WhenWeSerializeIt()
+        {
+            serializedResponse = JsonConvert.SerializeObject(originalResponse);
+        }
+
+        private void WhenWeDeserializeItAgain()
+        {
+            deserializedResponse = JsonConvert.DeserializeObject<Response>(serializedResponse);
+        }
+
+        private void ThenTheDeserializedResponseMatchesTheOriginalResponse()
+        {
+            deserializedResponse.Errors.Count.ShouldBe(originalResponse.Errors.Count);
+
+            foreach(var error in originalResponse.Errors)
+            {
+                deserializedResponse.Errors.ShouldContain(e => e.Code == error.Code && e.Message == error.Message);
+            }
+        }
+    }
+}
